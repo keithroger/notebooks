da0717a096f3076f3bfb88fd982ea80b1d3ebf91
keith <keithontiveros@ucla.edu>
2021-10-24 23:41:57 -0700

    Add dataset info

---
+++ Fraud Detection/fraud_detection.py
@@ -0,0 +1,9 @@
+# %%
+'''
+# Credit Card Fraud Detection
+
+The data used for this notebooks has already had principle component analysis
+(PCA) applied for confidentiality.
+
+Dataset available at https://www.kaggle.com/mlg-ulb/creditcardfraud
+'''
