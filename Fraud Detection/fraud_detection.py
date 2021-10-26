c0ebc0587d4b4159776c63631b458bc34ed143f5
keith <keithontiveros@ucla.edu>
2021-10-26 14:18:17 -0700

    read data and create pieplot

--- Fraud Detection/fraud_detection.py
+++ Fraud Detection/fraud_detection.py
@@ -7,3 +7,40 @@ The data used for this notebooks has already had principle component analysis
 
 Dataset available at https://www.kaggle.com/mlg-ulb/creditcardfraud
 '''
+
+# %%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %%
+df = pd.read_csv('data/creditcard.csv', header=0)
+df.head()
+
+# %%
+df.shape
+
+# %%
+df.dtypes
+
+# %%
+df.describe()
+
+# %%
+df.info()
+
+# %%
+fraud_percent = np.sum(df['Class']) / len(df['Class'])
+
+# %%
+fig, ax = plt.subplots()
+ax.pie([1.0 - fraud_percent, fraud_percent],
+       labels=['Non-Fraud', 'Fraud'],
+       autopct='%1.4f%%',
+       pctdistance=1.25,
+       labeldistance=1.55)
+plt.title('Fraud vs Non-Fraud')
+plt.savefig('images/pie.png')
+
+# %%
+
